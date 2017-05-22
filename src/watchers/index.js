959e977f320de7adb8923ce7bac3009a40aeee20
Thomas Alberola <thomas@needacoffee.fr>
2017-05-22 23:24:50 +0200

    Fixing lags, addding logs, add fun

--- src/watchers/index.js
+++ src/watchers/index.js
@@ -1,17 +1,16 @@
-const _    = require('lodash')
-const path = require('path')
+const _      = require('lodash')
+const path   = require('path')
+const moment = require('moment')
 
 module.exports = (client, channel, voiceConnection) => {
 
+  const helloFile = path.join(process.cwd(), 'src', 'assets', 'sounds', 'hello.mp3')
+
   client.on('voiceStateUpdate', (oldmember, newMember) => {
-
       if (newMember.voiceChannelID === channel.id) {
-        console.log(`${newMember.user.username} vient de se connecter au canal General.`)
 
-        const dispatcher = voiceConnection.playFile(path.join(process.cwd(), 'src', 'assets', 'sounds', 'hello.mp3'))
-        dispatcher.setVolume(0.5)
+        console.log(`${moment().format('MMMM Do YYYY, h:mm:ss a')} || ${newMember.user.username} vient de se connecter au canal General.`)
+        voiceConnection.playArbitraryInput(helloFile, { volume: 0.3 })
       }
-
-      //console.log('newMember',_.pick(newMember, ['voiceChannelID']))
   })
 }
